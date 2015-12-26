36ac717edec636058e9950bc34cdb881c6672951
Kazuki Saima <kazuki.saima@vapes.jp>
2015-12-27 04:45:35 +0900

    Add SearchBar

--- app/src/main/java/kazukisaima/kithub/MainActivity.kt
+++ app/src/main/java/kazukisaima/kithub/MainActivity.kt
@@ -3,6 +3,8 @@ package kazukisaima.kithub
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId ?: return true
+        if (id == R.id.menu_search) {
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }

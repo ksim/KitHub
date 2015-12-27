ce17525f871a9e09559ec768fc875f74ce409ede
Kazuki Saima <kazuki.saima@vapes.jp>
2015-12-27 10:19:52 +0900

    responseをRealmに保存

--- app/src/main/java/kazukisaima/kithub/presentation/activity/MainActivity.kt
+++ app/src/main/java/kazukisaima/kithub/presentation/activity/MainActivity.kt
@@ -2,44 +2,83 @@ package kazukisaima.kithub.presentation.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.ListViewCompat
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import butterknife.bindView
+import io.realm.Realm
+import io.realm.RealmChangeListener
 import kazukisaima.kithub.R
-import kazukisaima.kithub.model.moshi.SearchRepositoryResponse
+import kazukisaima.kithub.model.realm.Repository
+import kazukisaima.kithub.model.realm.User
+import kazukisaima.kithub.network.ApiClient
 import kazukisaima.kithub.network.ApiHelper
+import kazukisaima.kithub.network.GitHubApiService
+import rx.Observable
 import rx.Subscriber
+import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
+import rx.functions.Action1
 import rx.schedulers.Schedulers
 import timber.log.Timber
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+    val toolbar: Toolbar by bindView(R.id.toolbar)
+    val listView: ListViewCompat by bindView(R.id.listView)
+
+    private var realm: Realm by Delegates.notNull()
+    private var realmChangeListener: RealmChangeListener by Delegates.notNull()
+    private val api: GitHubApiService = ApiClient().getGitHubApiService()
+
+    private var subscription: Subscription by Delegates.notNull()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+        realm = Realm.getInstance(this)
+        realmChangeListener = RealmChangeListener {
+            Timber.d("data changed")
+        }
     }
 
     override fun onStart() {
         super.onStart()
-
-        ApiHelper().searchRepository("jquery")
+        subscription = api.searchRepositories("jquery")
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe(object: Subscriber<SearchRepositoryResponse>(){
-                override fun onNext(r: SearchRepositoryResponse?) {
-                    Timber.d(r.toString())
+            .subscribeOn(Schedulers.newThread())
+            .flatMap { Observable.from(it.items) }
+            .map {
+                realm.beginTransaction()
+                val user = it.owner.run {
+                    User(id, name, avatarURLString, urlString)
                 }
+                realm.copyToRealmOrUpdate(user)
 
-                override fun onError(e: Throwable?) {
-                    Timber.e(e.toString())
+                val repo = it.run {
+                    Repository(
+                        id, name, fullName, user, private, description, urlString,
+                        createDate, updateDate, stargazersCount, watchersCount, score
+                    )
                 }
+                realm.copyToRealmOrUpdate(repo)
+                realm.commitTransaction()
+                repo
+            }
+            .subscribe()
+    }
 
-                override fun onCompleted() {
-                }
-            })
+    override fun onStop() {
+        super.onStop()
+        subscription.unsubscribe()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
